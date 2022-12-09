a445601b4bb94ba9002d521bbb50884edb3f4fc5
KimKkochu <kimkkochu69@gmail.com>
2022-12-09 13:51:36 +0900

    8x8 RAM test

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from ram import *
 
-M = RAM()
+num = ''.join('{:0>8}'.format(format(54, 'b')))
 
-num = list(reversed("{:0>8}".format(format(54, 'b'))))
+s2, s1, s0 = map(int, '000')
 
-for i in range(8):
-   addr = "{:0>3}".format(format(i, 'b'))
-   M(int(addr[2]), int(addr[1]), int(addr[0]), int(num[i]), 1)
+_8x8_RAM =[RAM() for i in range(8)]
 
-stored_num = ''.join(list(reversed(M.stored())))
-print(stored_num)
-print(int(stored_num, 2))
+for i, j in zip(_8x8_RAM, num):
+    i(s2, s1, s0, D=int(j), W=1)
+
+for i in _8x8_RAM:
+    print(i(s2, s1, s0, D=0, W=0), end='')
+print()
