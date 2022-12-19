63f53786974653a7beea534e3af04e0ca7bbcbf8
KimKkochu <kimkkochu69@gmail.com>
2022-12-19 23:54:41 +0900

    Q error, line 17 (function)

--- ripple_counter.py
+++ ripple_counter.py
@@ -4,15 +4,22 @@ from ram import latch
 class Counter:
     def __init__(self, bit_quantity:int) -> None:
         self.Q = [latch() for i in range(bit_quantity-1)]
-        self.trigger = True
-        self._counter()
+        [self.Q[i].D_type_latch() for i in range(bit_quantity-1)]
+        self.Clk = False
+        self.Oscillator()
+        
+    def Oscillator(self):
+        self.Clk = not(self.Clk)
+        sleep(0.5)
+        print(self._counter(int(self.Clk)))
+        self.Oscillator()
 
-    def _counter(self):
-        Clk = False
-        while self.trigger:
-            # Oscillator
-            Clk = not(Clk)
-            sleep(0.3)
-            
-    def output(self, Clk_bar):
-        pass
+    def _counter(self, clk):
+        self.Q[0].D_type_latch(Clk=clk, D=self.Q[0].q_bar)
+        self.Q[1].D_type_latch(Clk=self.Q[0].q_bar, D=self.Q[1].q_bar)
+
+        return [self.Q[1].q, self.Q[0].q]
+        # self.Q[2].D_type_latch(Clk=self.Q[1].q_bar, D=self.Q[2].q_bar)
+        # return [self.Q[2].q ,self.Q[1].q, self.Q[0].q, int(not(clk))]
+
+Counter(3)
