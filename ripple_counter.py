68278b80c4cb9b95573b347149a5ec3dfcdab9fd
KimKkochu <kimkkochu69@gmail.com>
2022-12-19 22:25:21 +0900

    Oscillator in class

--- ripple_counter.py
+++ ripple_counter.py
@@ -1,8 +1,18 @@
 from time import sleep
+from ram import latch
 
-# Oscillator
-Clk = False
-while True:
-    print(int(Clk))
-    Clk = not(Clk)
-    sleep(0.3)
+class Counter:
+    def __init__(self, bit_quantity:int) -> None:
+        self.Q = [latch() for i in range(bit_quantity-1)]
+        self.trigger = True
+        self._counter()
+
+    def _counter(self):
+        Clk = False
+        while self.trigger:
+            # Oscillator
+            Clk = not(Clk)
+            sleep(0.3)
+            
+    def output(self, Clk_bar):
+        pass
